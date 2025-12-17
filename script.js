5ff2e1d7af5b4374665ee79f39795f3fc6466484
Arina Shmodina <ApelsinGilka@yandex.ru>
2025-12-18 02:39:46 +0300

    bugfix

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const btn = document.querySelector("#theme-toggle");
 const page = document.documentElement;
 
 btn.addEventListener("click", () => {
-    const theme = page.getAttribute("data-theme") === "light"
-        ? "dark"
-        : "light"
+    const theme = page.getAttribute("data-theme") === "dark"
+        ? "light"
+        : "dark"
 
         page.setAttribute("data-theme", theme)
         console.log('BOBER');
